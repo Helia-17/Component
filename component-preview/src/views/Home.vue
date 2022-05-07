1433e84b6523ce3393d6c4e5b08b93a663f12ecc
Helia <jyk.helia@gmail.com>
2022-05-07 11:49:47 +0900

    build: added vue-router

---
+++ component-preview/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/puzzle-piece.png" width="200" />
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
