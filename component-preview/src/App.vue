067ee804fb528bf11b13b9e37aa98aac781fc3a4
Helia <jyk.helia@gmail.com>
2022-05-07 10:29:03 +0900

    chore: changed logo and description

--- component-preview/src/App.vue
+++ component-preview/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Helia logo" src="./assets/puzzle-piece.png" width="200" />
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
